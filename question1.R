cfa5d4f5990f360766ec27244d9a5bdeeff4eb6a
SiddharthPanda <siddharth.panda16@gmail.com>
2018-10-08 21:57:22 -0500

    Added Labels and Updated Nodes
    Added X and Y labels and updated the sell plot to FROMNODE vs AMOUNT
    and buyer plot to TONODE vs AMOUNT

--- question1.R
+++ question1.R
@@ -1,4 +1,4 @@
-file<- "B:/study/Fall 2018/Stats for DS/Project1/Datasets/Ethereum token graphs/networkbnbTX.txt"
+file<- "D:/UTD/Academics/Fourth Sem/Stats for Data Science - Cuneyt/Project1/Token Graphs/networkbnbTX.txt"
 col_names <- c("FROMNODE","TONODE","TIME","TOKENAMOUNT")
 mydata <- read.csv( file, header = FALSE, sep = " ", dec = ".", col.names = col_names)
 amounts <- mydata[4]
@@ -23,14 +23,16 @@ print(count)
 
 #graph1
 sells <- mydata[1]
-
+plot(x=mydata$FROMNODE, y= mydata$TOKENAMOUNT, main="Number of tokens sold by addresses", sub="Sellers",xlab="FROM NODE", ylab="TOKEN AMOUNT")
 
 #graph2
 buys <- mydata[2]
-g_range <- range(0, mydata$V2)
-plot(x=mydata$FROMNODE, y= mydata$TOKENAMOUNT, title(main="Number of tokens bought by addresses", sub="Buyers", ylab="Tokens"))
-#par(tcl= -0.2)
-#axis(1, at=100*g_range[1], labels=F, lwd=1, lwd.ticks=1)
-#par(tcl= -0.5)
-#axis(1, at=10000*g_range[1], labels=seq(0,g_range[1],100), lwd=0, lwd.ticks=2)
+plot(x=mydata$TONODE, y= mydata$TOKENAMOUNT, main="Number of tokens bought by addresses", sub="Buyers",xlab="TO NODE", ylab="TOKEN AMOUNT")
+# 
+# g_range <- range(0, mydata$FROMNODE)
+# print(g_range)
+# par(tcl= -0.2)
+# axis(1, at=100*g_range[1], labels=F, lwd=1, lwd.ticks=1)
+# par(tcl= -0.5)
+# axis(1, at=10000*g_range[1], labels=seq(0,g_range[1],100), lwd=0, lwd.ticks=2)
 
