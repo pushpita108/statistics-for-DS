f84caf299d891601f0f7de9bfc794bcf0fbf86cd
Akash Chand <akash.chand6891@gmail.com>
2018-10-08 19:35:22 -0500

    Code for plotting added

--- question1.R
+++ question1.R
@@ -1,5 +1,6 @@
-file<- "/Users/pushpitapanigrahi/Desktop/PushpitaFiles/Study/4.StatsForDS/Proj1/Selected/networkbnbTX.txt"
-mydata <- read.csv( file, header = FALSE, sep = " ", dec = ".")
+file<- "B:/study/Fall 2018/Stats for DS/Project1/Datasets/Ethereum token graphs/networkbnbTX.txt"
+col_names <- c("FROMNODE","TONODE","TIME","TOKENAMOUNT")
+mydata <- read.csv( file, header = FALSE, sep = " ", dec = ".", col.names = col_names)
 amounts <- mydata[4]
 
 totalSupply <- 192443301
@@ -26,3 +27,10 @@ sells <- mydata[1]
 
 #graph2
 buys <- mydata[2]
+g_range <- range(0, mydata$V2)
+plot(x=mydata$FROMNODE, y= mydata$TOKENAMOUNT, title(main="Number of tokens bought by addresses", sub="Buyers", ylab="Tokens"))
+#par(tcl= -0.2)
+#axis(1, at=100*g_range[1], labels=F, lwd=1, lwd.ticks=1)
+#par(tcl= -0.5)
+#axis(1, at=10000*g_range[1], labels=seq(0,g_range[1],100), lwd=0, lwd.ticks=2)
+
